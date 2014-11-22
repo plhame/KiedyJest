0d82d9dd15b3f56f47056ca2c642ce0eb579e0ae
Tomasz Rogozik <tomasz.rogozik@gmail.com>
2014-11-22 22:14:05 +0100

    Initial MVC project structure.

---
+++ KiedyJestWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KiedyJestWeb.Startup))]
+namespace KiedyJestWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
