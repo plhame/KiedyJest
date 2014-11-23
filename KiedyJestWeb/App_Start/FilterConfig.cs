c3d428b45e12f199d219063a38b7f5967c60cdc1
Tomasz Rogozik <tomasz.rogozik@gmail.com>
2014-11-23 19:29:31 +0100

    Cleaning up the project of unused dependencies and identity management (the website will not use any kind of authentication).

--- KiedyJestWeb/App_Start/FilterConfig.cs
+++ KiedyJestWeb/App_Start/FilterConfig.cs
@@ -1,8 +1,7 @@
-﻿using System.Web;
-using System.Web.Mvc;
-
-namespace KiedyJestWeb
+﻿namespace KiedyJestWeb
 {
+    using System.Web.Mvc;
+
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
