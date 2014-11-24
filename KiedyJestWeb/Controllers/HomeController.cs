53129b3977be254cbf1fda6eb747e2bbeff6bc12
Tomasz Rogozik <tomasz.rogozik@gmail.com>
2014-11-24 22:34:36 +0100

    Adding NLog to the project. Enabling logging to event log and ASP trace.

--- KiedyJestWeb/Controllers/HomeController.cs
+++ KiedyJestWeb/Controllers/HomeController.cs
@@ -2,11 +2,15 @@
 {
     using System.Web.Mvc;
 
+    using NLog;
+
     /// <summary>
     /// Represents the main page controller.
     /// </summary>
     public class HomeController : Controller
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public ActionResult Index()
         {
             return View();
